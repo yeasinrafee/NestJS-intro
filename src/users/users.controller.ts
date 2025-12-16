1d73862afd96e2253c8c4ef2550460b6cf1bdb7c
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-12-16 10:01:41 +0600

    Params, Query and body decorators

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  public getUsers() {
+  @Get('/:id')
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params);
+    console.log(query);
     return 'You sent a get request to users endpoint';
   }
 
   @Post()
-  public createUsers() {
+  public createUsers(@Body() body: any) {
+    console.log(body);
     return 'You sent a post request to users endpoint';
   }
 }
