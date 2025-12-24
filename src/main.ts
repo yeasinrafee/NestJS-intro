bf25b9f48fe69f983591e9ff4f223c67e4065fd5
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-12-24 23:57:22 +0600

    Converting an instance of DTO

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
